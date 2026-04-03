02417469a5f3bdd7c2200218f7f39406a120b042
Freddie <119540226+SanJammin@users.noreply.github.com>
2026-04-03 11:58:40 +0100

    feat: create function getHeadingFromHTML

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,8 +1,20 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(url: string): string {
     const urlObj = new URL(url);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
     return hostPath.toLowerCase().replace(/\/$/, "");
 }
 
-// REMOVE THIS AFTER TESTING
-// console.log(normalizeURL("https://www.boot.dev/blog/path/"));
+export function getHeadingFromHTML(html: string): string {
+    const dom = new JSDOM(html);
+    const h1 = dom.window.document.querySelector("h1");
+    const h2 = dom.window.document.querySelector("h2");
+    if (h1) {
+        return h1.textContent?.trim() || "";
+    } else if (h2) {
+        return h2.textContent?.trim() || "";
+    } else {
+        return "";
+    }
+}
