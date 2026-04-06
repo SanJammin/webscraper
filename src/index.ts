47de50e7cffb94af36f3f1eafa8fb14f58fa88d9
Freddie <119540226+SanJammin@users.noreply.github.com>
2026-04-06 17:22:52 +0100

    feat: create main function that will run programme

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function main() {
+    const args = process.argv.slice(2);
+    if (args.length > 1) {
+        console.error("Too many arguments provided.");
+        process.exit(1);
+    } else if (args.length < 1) {
+        console.error("No argument provided.");
+        process.exit(1);
+    }
+    
+    const baseURL = args[0];
+    console.log(`Crawler is starting at base URL: ${baseURL}`);
+    process.exit(0);
+}
+
+main();
