7cf832c31e7f24491f7485ee9d1e3578f748dfa0
Freddie <119540226+SanJammin@users.noreply.github.com>
2026-04-11 14:14:35 +0100

    refactor: handle increased argument logic

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import { crawlSiteAsync } from "./crawl.js";
 
 async function main() {
     const args = process.argv.slice(2);
-    if (args.length > 1) {
+    if (args.length > 3) {
         console.error("Too many arguments provided.");
         process.exit(1);
-    } else if (args.length < 1) {
-        console.error("No argument provided.");
+    } else if (args.length < 3) {
+        console.error("Insufficient arguments provided.");
         process.exit(1);
     }
 
     const baseURL = args[0];
+    const maxConcurrency = parseInt(args[1]);
+    const maxPages = parseInt(args[2]);
+
     console.log(`Crawler is starting at base URL: ${baseURL}`);
-    console.log(await crawlSiteAsync(baseURL, 10));
+    console.log(await crawlSiteAsync(baseURL, maxConcurrency, maxPages));
 }
 
 main();
